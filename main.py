7c7bdd944d2fdb0646680f93aa55280b66918764
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-26 14:33:52 -0500

    Day Changing

--- main.py
+++ main.py
@@ -1,20 +1,60 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel
 import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
+from PySide6.QtCore import QDate
+from PySide6.QtGui import QIcon, Qt
 
 class WeatherDashboard(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Weather Dashboard")
-        self.resize(400, 300)
+        self.resize(600, 350)
 
         centralWidget = QWidget()
         self.setCentralWidget(centralWidget)
 
         layout = QVBoxLayout(self)
+        header_layout = QHBoxLayout()
+
+        layout.addLayout(header_layout)
+
+        self.currentDate = QDate.currentDate()
+
+        self.date_label = QLabel()
+        self.date_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        left_date_button = QPushButton()
+        right_date_button = QPushButton()
+
+        left_date_button.setIcon(QIcon("icons/leftArrow.svg"))
+        right_date_button.setIcon(QIcon("icons/rightArrow.svg"))
+
+        left_date_button.clicked.connect(self.previous_date)
+        right_date_button.clicked.connect(self.next_date)
+
+        self.update_date()
+
+        header_layout.addWidget(left_date_button)
+        header_layout.addWidget(self.date_label)
+        header_layout.addWidget(right_date_button)
+
         centralWidget.setLayout(layout)
 
-        title = QLabel("Test")
-        layout.addWidget(title)
+    def update_date(self):
+        date_string = self.currentDate.toString("dddd, MMMM, d")
+
+        self.date_label.setText(date_string)
+
+        self.setWindowTitle(f"Weather Dashboard - {date_string}")
+
+    def previous_date(self):
+        self.currentDate = self.currentDate.addDays(-1)
+        self.update_date()
+
+    def next_date(self):
+        self.currentDate = self.currentDate.addDays(1)
+        self.update_date()
+
+
+
 
 app = QApplication(sys.argv)
 
