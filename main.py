0bd82b36fc2c76fa44ce3d4d6df2b750400ce96a
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-26 09:23:47 -0500

    WeatherDashboard class

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel
 import sys
+
+class WeatherDashboard(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Weather Dashboard")
+        self.resize(400, 300)
+
+        centralWidget = QWidget()
+        self.setCentralWidget(centralWidget)
+
+        layout = QVBoxLayout(self)
+        centralWidget.setLayout(layout)
+
+        title = QLabel("Test")
+        layout.addWidget(title)
 
 app = QApplication(sys.argv)
 
-window = QMainWindow()
-window.setWindowTitle("Weather Dashboard")
-window.resize(400, 300)
+window = WeatherDashboard()
 
 window.show()
 app.exec()
